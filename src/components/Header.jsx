0d7ded32ed297be88353608166a987d24c0254fb
polozovaanastasia <polozova.anastasia@gmail.com>
2021-05-02 17:41:08 +0500

    Revert "test"
    This reverts commit d5d7af84c1d674ebab865948ef819beb5828c9a7.

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-const Header = () => {
-    return (
-        <header className="header">
-            <img className="logo" src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" alt="logo" />
-        </header>
-    );
-}
-export default Header;
