d5d7af84c1d674ebab865948ef819beb5828c9a7
polozovaanastasia <polozova.anastasia@gmail.com>
2021-05-02 16:56:52 +0500

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="logo" src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" alt="logo" />
+        </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+    return (
+        <main className="content">
+            <div className="cover_img">
+                <img src="https://assets.about.me/background/users/t/a/l/talitabacetti_1400684030_05.jpg" alt="" />
+            </div>
+            <div>
+                <img className="user_avatar" src="https://html5css.ru/w3css/img_avatar3.png" alt="" />
+                <div className="user_info"></div>
+            </div>
+            <div>
+                My posts
+          <div>
+                    New post
+          </div>
+            </div>
+            <div>
+                <div>
+                    Post 1
+          </div>
+                <div>
+                    Post 2
+          </div>
+            </div>
+        </main>
+    );
+}
+export default Profile;
