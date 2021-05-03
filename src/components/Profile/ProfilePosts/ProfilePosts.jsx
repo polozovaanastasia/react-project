843bf24fc3e28e7416b15822ab55c4e0eabb275b
polozovaanastasia <polozova.anastasia@gmail.com>
2021-05-03 18:12:38 +0500

    убираю ворнинг из консоли.

--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -5,7 +5,7 @@ const ProfilePosts = () => {
     return (
         <div>
             <h2>My posts</h2>
-            <textarea name="" id="" cols="100" rows="3">New post</textarea>
+            <textarea name="" id="" defaultValue="New post" cols="100" rows="3" ></textarea> 
             <button>Add post</button>
             <ProfilePost message="Hi, how are you?" img="https://html5css.ru/w3images/avatar2.png" />
             <ProfilePost message="It's my first post" img="https://html5css.ru/w3images/avatar6.png" />
