a3db3cb5b04a2ea6d90ba92db259795c9dbfcebe
polozovaanastasia <polozova.anastasia@gmail.com>
2021-05-04 20:56:30 +0500

    накидываю верстку Messages.

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,9 +1,39 @@
-// import classes from './Messages.module.css';
+import classes from './Messages.module.css';
 
 const Messages = () => {
     return (
-        <div>
-            Messages
+        <div className={classes.dialogs}>
+            <div className={classes.users}>
+                <div className={classes.users_items}>
+                    <div className={classes.user + ' ' + classes.user_active}>
+                        Name
+                    </div>
+
+                    <div className={classes.user}>
+                        Name
+                    </div>
+
+                    <div className={classes.user}>
+                        Name
+                    </div>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.messages_items}>
+                    <div className={classes.message}>
+                        <img src="https://html5css.ru/w3images/avatar2.png" alt="avatar" className={classes.avatar} />
+                        <span className={classes.message_text}>Hi, how are you?</span>
+                    </div>
+                </div>
+
+                <div className={classes.messages_items}>
+                    <div className={classes.message}>
+                        <img src="https://html5css.ru/w3images/avatar2.png" alt="avatar" className={classes.avatar} />
+                        <span className={classes.message_text}>Hi, how are you?</span>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
