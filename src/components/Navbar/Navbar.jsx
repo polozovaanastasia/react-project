850e81f1d8b3ff9e5c2112ec777bf7214d501e7b
polozovaanastasia <polozova.anastasia@gmail.com>
2021-05-04 19:55:34 +0500

    начинаю использовать NavLink.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item + ' ' + classes.active}>
-                <a href="/profile">Profile</a>
+            <div className={classes.item}>
+                <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/messages">Messages</a>
+                <NavLink to="/messages" activeClassName={classes.active}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news" activeClassName={classes.active}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/music">Music</a>
+                <NavLink to="/music" activeClassName={classes.active}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/settings">Settings</a>
+                <NavLink to="/settings" activeClassName={classes.active}>Settings</NavLink>
             </div>
         </nav>
     );
