cc60aedefab6dc35b18ed063d268ddc9ad6f1927
polozovaanastasia <polozova.anastasia@gmail.com>
2021-08-31 22:01:16 +0500

    начинаю использовать события и рефы.

--- src/components/Profile/ProfilePosts/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts/ProfilePosts.jsx
@@ -1,12 +1,20 @@
+import React from 'react';
 import classes from './ProfilePosts.module.css';
+
 const ProfilePosts = (props) => {
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+        console.log(text)
+    };
 
     return (
         <div className={classes.posts}>
             <h2>My posts</h2>
-            <textarea name="" id="" defaultValue="New post" cols="100" rows="3" ></textarea> 
-            <button>Add post</button>
-            { props.posts }
+            <textarea name="" ref={newPostElement} defaultValue="New post" cols="100" rows="3" ></textarea>
+            <button onClick={addPost}>Add post</button>
+            {props.posts}
         </div>
     );
 }
