93008035f8392604e47e06b983a0297f76130ecb
polozovaanastasia <polozova.anastasia@gmail.com>
2021-11-04 12:01:13 +0500

    переношу мапинг юзеров и диалогов из контейнерной компоненты.

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,8 +1,13 @@
 import classes from './Messages.module.css';
+import User from './Users/User/User';
+import Dialog from './Dialogs/Dialog/Dialog';
 import Users from './Users/Users';
 import Dialogs from './Dialogs/Dialogs';
 
 const Messages = (props) => {
+    let usersElements = props.state.users.map(user => (<User avatar={user.avatar} name={user.name} id={user.id} key={user.id} />));
+    let dialogsElements = props.state.dialogs.map(dialog => (<Dialog avatar={dialog.avatar} message={dialog.message} direction={dialog.direction} key={dialog.id} />));
+
     let onAddNewMessage = () => {
         props.addNewMessage();
     };
@@ -14,11 +19,11 @@ const Messages = (props) => {
     return (
         <div className={classes.messages}>
             <div className={classes.users}>
-                <Users users={props.usersElements} />
+                <Users users={usersElements} />
             </div>
             <div className={classes.dialogs}>
                 <Dialogs
-                    dialogs={props.dialogsElements} />
+                    dialogs={dialogsElements} />
 
                 <div className={classes.dialogs_new}>
                     <textarea onChange={onNewMessageChange} value={props.newMessageText} cols="100" rows="3"></textarea>
--- src/components/Messages/MessagesContainer.jsx
+++ src/components/Messages/MessagesContainer.jsx
@@ -1,14 +1,9 @@
 import { addNewMessageActionCreator, updateNewMessageActionCreator } from '../../redux/messagesReducer';
 import Messages from "./Messages";
-import User from './Users/User/User';
-import Dialog from './Dialogs/Dialog/Dialog';
 
 const MessagesContainer = (props) => {
     let state = props.store.getState().messagesPage;
     let newMessageText = state.newMessageText;
-    
-    let usersElements = state.users.map(user => (<User avatar={user.avatar} name={user.name} id={user.id} key={user.id} />));
-    let dialogsElements = state.dialogs.map(dialog => (<Dialog avatar={dialog.avatar} message={dialog.message} direction={dialog.direction} key={dialog.id} />));
     
     let addNewMessage = () => { //if(event.key === 'Enter' && !event.shiftKey)
         props.store.dispatch(addNewMessageActionCreator())
@@ -19,8 +14,7 @@ const MessagesContainer = (props) => {
 
     return (
         <Messages 
-            usersElements={usersElements}
-            dialogsElements={dialogsElements} 
+        state={state} 
             newMessageText={newMessageText}
             addNewMessage={addNewMessage}
             updateNewMessage={updateNewMessage} />
